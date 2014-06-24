13a90c653a0421ff9ff7dde06ef831e0013c8d00
Amoki <hugoduroux@gmail.com>
2014-06-24 16:57:01 +0200

    SUPER CONNARD

--- remote_url_opener/settings.py
+++ remote_url_opener/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'remote_url_opener',
+    'browser',
 )
 
 MIDDLEWARE_CLASSES = (
