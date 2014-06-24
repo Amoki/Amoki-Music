285d35e4bbf478f69edc4554b15305bdf20622f0
Amoki <hugoduroux@gmail.com>
2014-06-24 15:31:13 +0200

    typo

--- remote_url_opener/urls.py
+++ remote_url_opener/urls.py
@@ -5,5 +5,5 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('broswer.urls')),
+    url(r'^', include('browser.urls')),
 )
