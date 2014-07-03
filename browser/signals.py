5315056da6671ec351c3b086ee8692a4a7ec46c7
Eirika <pitchetrou@msn.com>
2014-07-04 01:04:15 +0200

    reverting

--- browser/signals.py
+++ browser/signals.py
@@ -4,7 +4,7 @@ from django.dispatch import receiver
 
 from browser.models import Music
 
-import urllib
+import urllib2
 import json
 
 
@@ -23,8 +23,8 @@ def get_youtube_link(id):
 def set_url_name_and_duration(sender, instance, **kwargs):
     video_id = get_youtube_id(instance.url)
     query = "https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails&id=" + video_id + "&key=AIzaSyCt5t3qv1MTXW5Vaq0KB9__0m7xP5bQNo4"
-    body = urllib.request.urlopen(query).read()
-    res = json.loads(body.decode())
+    body = urllib2.urlopen(query).read()
+    res = json.loads(body)
 
     raw_time = res["items"][0]["contentDetails"]["duration"]
     time = int(raw_time.rsplit("M", 1)[1].rsplit("S", 1)[0])
