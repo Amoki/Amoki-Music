9924efb90bda854890469c90280eb6084002a2f7
Amoki <hugoduroux@gmail.com>
2014-07-04 02:03:45 +0200

    refix time"

--- browser/models.py
+++ browser/models.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 import webbrowser
 import psutil
 from datetime import datetime
+from datetime import timedelta
 from threading import Timer
 
 
@@ -104,7 +105,7 @@ class Player(models.Model):
     def get_actual_remaining_time(self):
         if not self.actual:
             return 0
-        return (datetime.now() - self.actual.date).total_seconds()
+        return self.actual.duration - ((datetime.now() - self.actual.date)).total_seconds()
 
     def get_remaining_time(self):
         if not self.actual:
