4f0f835f15b527bd66408671ebb616d61fa83c83
Eirika <pitchetrou@msn.com>
2014-10-09 17:26:22 +0200

    fix btn-shuffle style inactive

---
+++ amoki_music/templates/assets/js/usefullFunction.js
@@ -0,0 +1,33 @@
+$( document ).ready(function() {
+	var hauteur = $(window).height() - 110;
+	$(".LIB").height(hauteur);
+	$(".player").height(hauteur - 50);
+	$(".list-lib").height(hauteur - 100);
+	$(".panel-playlist").height(hauteur - 300);     
+	$( window ).resize(function() {
+		if ($(window).width() > 992) {
+			if ($(window).height() > 500) {
+				var hauteur = $(window).height() - 110;
+				$(".LIB").height(hauteur);
+				$(".player").height(hauteur - 50);
+				$(".list-lib").height(hauteur - 100);
+				$(".panel-playlist").height(hauteur - 300); 
+			}
+		}
+	});
+
+
+	//Override de la fonction "contains" pour permettre une recherche case-insensitive
+	jQuery.expr[':'].contains = jQuery.expr.createPseudo(function(arg) {
+		return function( elem ) {
+			return jQuery(elem).text().toUpperCase().indexOf(arg.toUpperCase()) >= 0;
+		};
+	});
+	//Recherche basée sur le champs input "input-search"
+	$("#input-search").keyup(function(){
+		var strToSearch = $("#input-search").val();
+		$(".list-music:not(:contains('"+strToSearch+"'))").slideUp();
+		$(".list-music:contains('"+strToSearch+"')").slideDown();
+	});
+
+});
