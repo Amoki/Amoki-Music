b8807a5c722fd1fda5ee3cd5ed9543e768a51bd1
Eirika <pitchetrou@msn.com>
2014-10-09 18:50:30 +0200

    Add cover player + fixed weird variable height

--- amoki_music/templates/assets/js/usefullFunction.js
+++ amoki_music/templates/assets/js/usefullFunction.js
@@ -1,17 +1,17 @@
 $( document ).ready(function() {
 	var hauteur = $(window).height() - 110;
 	$(".LIB").height(hauteur);
-	$(".player").height(hauteur - 50);
+//	$(".player").height(hauteur - 50);
 	$(".list-lib").height(hauteur - 100);
-	$(".panel-playlist").height(hauteur - 300);     
+//	$(".panel-playlist").height(hauteur - 300);     
 	$( window ).resize(function() {
 		if ($(window).width() > 992) {
 			if ($(window).height() > 500) {
 				var hauteur = $(window).height() - 110;
 				$(".LIB").height(hauteur);
-				$(".player").height(hauteur - 50);
+				//$(".player").height(hauteur - 50);
 				$(".list-lib").height(hauteur - 100);
-				$(".panel-playlist").height(hauteur - 300); 
+				//$(".panel-playlist").height(hauteur - 300); 
 			}
 		}
 	});
