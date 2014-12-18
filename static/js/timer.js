fabdadedaaf179bfc0888d615eb658c67dcee4a4
Eirika <pitchetrou@msn.com>
2014-12-18 23:52:13 +0100

    Change indent

--- static/js/timer.js
+++ static/js/timer.js
@@ -1,120 +1,120 @@
 var myCounter = new Countdown({
-	onCounterEnd: function(){update_player();}
+  onCounterEnd: function(){update_player();}
 });
 
 function timeline(current_time_left, current_time_past_percent){
-	$(".progress-bar").stop();
-	var actual_time = current_time_left;
-	actual_time *= 1000;
-	$(".progress-bar").css('width', current_time_past_percent+'%');
-	$(".progress-bar").animate({ 'width' : '100%'} , {
-    	duration: actual_time,
-    	easing: 'linear',
-    	complete:function() {}
-	});
+  $(".progress-bar").stop();
+  var actual_time = current_time_left;
+  actual_time *= 1000;
+  $(".progress-bar").css('width', current_time_past_percent+'%');
+  $(".progress-bar").animate({ 'width' : '100%'} , {
+      duration: actual_time,
+      easing: 'linear',
+      complete:function() {}
+  });
 }
 
 
 function Countdown(options) {
-	var timer,
-	instance = this
-	counterEnd = options.onCounterEnd || function () {};
-	
-	function decrementCounter() {
-		var elementDisplay = $('.time-left-progress-bar');
-		var heures = Math.floor(seconds / 3600);
-		var minutes = Math.floor((seconds - (heures * 3600)) / 60);
-		var secondes = seconds - (heures * 3600) - (minutes * 60) ;
-		var printedTime = "";
-		if(secondes === 0) {
-			secondes = 59;
-			if(minutes === 0) {
-				minutes = 59;
-				if(heures === 0){
-					counterEnd();
-					instance.stop();
-				} else {
-					heures--;
-				}
-			} else {
-				minutes--;
-			}
-		} else {
-			secondes--;
-		}
-		var stantardize = function(num){
-			if (num < 10) {
-				printedTime += "0";
-			}
-		};
-		if(heures !== 0){
-			printedTime = heures+":";
-			stantardize(minutes);
-		}
-		printedTime += minutes+":";
-		stantardize(secondes);
-		printedTime += secondes;
-		elementDisplay.html(printedTime);
+  var timer,
+  instance = this
+  counterEnd = options.onCounterEnd || function () {};
+  
+  function decrementCounter() {
+    var elementDisplay = $('.time-left-progress-bar');
+    var heures = Math.floor(seconds / 3600);
+    var minutes = Math.floor((seconds - (heures * 3600)) / 60);
+    var secondes = seconds - (heures * 3600) - (minutes * 60) ;
+    var printedTime = "";
+    if(secondes === 0) {
+        secondes = 59;
+        if(minutes === 0) {
+            minutes = 59;
+            if(heures === 0){
+                counterEnd();
+                instance.stop();
+            } else {
+                heures--;
+            }
+        } else {
+            minutes--;
+        }
+    } else {
+        secondes--;
+    }
+    var stantardize = function(num){
+        if (num < 10) {
+            printedTime += "0";
+        }
+    };
+    if(heures !== 0){
+        printedTime = heures+":";
+        stantardize(minutes);
+    }
+    printedTime += minutes+":";
+    stantardize(secondes);
+    printedTime += secondes;
+    elementDisplay.html(printedTime);
 
-		seconds--;
-	}
+    seconds--;
+  }
 
-	this.start = function (param_sec) {
-		clearInterval(timer);
-		timer = 0;
-		seconds = param_sec;
-		if (seconds === 0) {
-		  return;
-		}
-		decrementCounter();
-		timer = setInterval(decrementCounter, 1000);
-	};
+  this.start = function (param_sec) {
+      clearInterval(timer);
+      timer = 0;
+      seconds = param_sec;
+      if (seconds === 0) {
+        return;
+      }
+      decrementCounter();
+      timer = setInterval(decrementCounter, 1000);
+  };
 
-	this.stop = function () {
-		var elementDisplay = $('.time-left-progress-bar');
-		elementDisplay.html("");
-		clearInterval(timer);
-	};
+  this.stop = function () {
+      var elementDisplay = $('.time-left-progress-bar');
+      elementDisplay.html("");
+      clearInterval(timer);
+  };
 }
 
 function updateDisplayTimeLeft(sec){
-	
+  
 
-	return false;
+  return false;
 }
 
 function updateTimePlaylistTemporaryFunction(sec){
-	var heures = Math.floor(sec / 3600);
-	var minutes = Math.floor((sec - (heures * 3600)) / 60);
-	var secondes = sec - (heures * 3600) - (minutes * 60) ;
-	var printedTime = "";
-	if(secondes === 0) {
-		secondes = 59;
-		if(minutes === 0) {
-			minutes = 59;
-			if(heures === 0){
-				return '00:00';
-			} else {
-				heures--;
-			}
-		} else {
-			minutes--;
-		}
-	} else {
-		secondes--;
-	}
-	var stantardize = function(num){
-		if (num < 10) {
-			printedTime += "0";
-		}
-	};
-	if(heures !== 0){
-		printedTime = heures+":";
-		stantardize(minutes);
-	}
-	printedTime += minutes+":";
-	stantardize(secondes);
-	printedTime += secondes;
+  var heures = Math.floor(sec / 3600);
+  var minutes = Math.floor((sec - (heures * 3600)) / 60);
+  var secondes = sec - (heures * 3600) - (minutes * 60) ;
+  var printedTime = "";
+  if(secondes === 0) {
+      secondes = 59;
+      if(minutes === 0) {
+          minutes = 59;
+          if(heures === 0){
+              return '00:00';
+          } else {
+              heures--;
+          }
+      } else {
+          minutes--;
+      }
+  } else {
+      secondes--;
+  }
+  var stantardize = function(num){
+      if (num < 10) {
+          printedTime += "0";
+      }
+  };
+  if(heures !== 0){
+      printedTime = heures+":";
+      stantardize(minutes);
+  }
+  printedTime += minutes+":";
+  stantardize(secondes);
+  printedTime += secondes;
 
-	return printedTime;
+  return printedTime;
 }
--- static/js/usefullFunction.js
+++ static/js/usefullFunction.js
@@ -1,79 +1,79 @@
 $( document ).ready(function() {
-	var hauteur = $(window).height() - 115;
-	if ($(window).height() > 765) {
-		hauteur = $(window).height() - 115;
-	} else {
-		hauteur = 650;
-	}
-	$(".LIB").height(hauteur);
-	$(".list-lib").height(hauteur - 90);
-	$(".tab-content").height(hauteur - 130);
-	$( window ).resize(function() {
-		if ($(window).width() > 992) {
-			if ($(window).height() > 765) {
-				hauteur = $(window).height() - 115;
-			} else {
-				hauteur = 650;
-			}
-			$(".LIB").height(hauteur);
-			$(".list-lib").height(hauteur - 90);
-			$(".tab-content").height(hauteur - 130);
-		}
-	});
+  var hauteur = $(window).height() - 115;
+  if ($(window).height() > 765) {
+      hauteur = $(window).height() - 115;
+  } else {
+      hauteur = 650;
+  }
+  $(".LIB").height(hauteur);
+  $(".list-lib").height(hauteur - 90);
+  $(".tab-content").height(hauteur - 130);
+  $( window ).resize(function() {
+      if ($(window).width() > 992) {
+          if ($(window).height() > 765) {
+              hauteur = $(window).height() - 115;
+          } else {
+              hauteur = 650;
+          }
+          $(".LIB").height(hauteur);
+          $(".list-lib").height(hauteur - 90);
+          $(".tab-content").height(hauteur - 130);
+      }
+  });
 
-    $(".btn").click(function(){
-	    $(this).blur();
-	});
+  $(".btn").click(function(){
+      $(this).blur();
+  });
 
-	updateDataTime();
-	$('.ajax_music_inifite_scroll').submit();
+  updateDataTime();
+  $('.ajax_music_inifite_scroll').submit();
 });
 
 function maj_header_player(data){
-	if(data.current_music){
-		$(document).attr('title', data.current_music[0].fields.name);
-		$('#url-next').val(data.current_music[0].fields.url);
-		$('#url-dead-link').val(data.current_music[0].fields.url);
-	} else {
-		disabled_btn();
-	}
-	$(".player").children('.header-player').html(data.template_header_player);
+  if(data.current_music){
+      $(document).attr('title', data.current_music[0].fields.name);
+      $('#url-next').val(data.current_music[0].fields.url);
+      $('#url-dead-link').val(data.current_music[0].fields.url);
+  } else {
+      disabled_btn();
+  }
+  $(".player").children('.header-player').html(data.template_header_player);
 }
 
 function disabled_btn(){
-	$(document).attr('title', 'Amoki\'s musics');
-	$(".header-player").children().remove();
-	$('.header-player').append('<div class="col-md-12 title"><div class="marquee"><span class="now-playing">No music :\'( Add yours now !</span></div></div>');
-	$("#btn-next").attr('disabled', 'disabled');
-	$("#dead-link").attr('disabled', 'disabled');
-	$(".progress-bar").stop();
-	$(".progress-bar").css('width', '0%');
-	myCounter.stop();
+  $(document).attr('title', 'Amoki\'s musics');
+  $(".header-player").children().remove();
+  $('.header-player').append('<div class="col-md-12 title"><div class="marquee"><span class="now-playing">No music :\'( Add yours now !</span></div></div>');
+  $("#btn-next").attr('disabled', 'disabled');
+  $("#dead-link").attr('disabled', 'disabled');
+  $(".progress-bar").stop();
+  $(".progress-bar").css('width', '0%');
+  myCounter.stop();
 }
 
 function maj_playlist_current(data){
-	myCounter.stop();
-	$("#btn-next").removeAttr('disabled');
-	$("#dead-link").removeAttr('disabled');
-	$('.playlist-ajax').html(data.template_playlist);
-	updateDataTime();
-	maj_header_player(data);
-	if(data.current_music){
-		myCounter.start(data.time_left);
-	}
+  myCounter.stop();
+  $("#btn-next").removeAttr('disabled');
+  $("#dead-link").removeAttr('disabled');
+  $('.playlist-ajax').html(data.template_playlist);
+  updateDataTime();
+  maj_header_player(data);
+  if(data.current_music){
+      myCounter.start(data.time_left);
+  }
 }
 
 function modal_confirm(target){
-	target.modal({
-		'show':true,
-		'backdrop':false
-	}).on('shown.bs.modal', function(){
-		setTimeout(function(){target.modal('hide');}, 1000);
-	});
+  target.modal({
+      'show':true,
+      'backdrop':false
+  }).on('shown.bs.modal', function(){
+      setTimeout(function(){target.modal('hide');}, 1000);
+  });
 }
 
 function updateDataTime(){
-	$('span[data-time]').each(function(){
-       $(this).html(updateTimePlaylistTemporaryFunction($(this).data("time")));
-	});
+  $('span[data-time]').each(function(){
+     $(this).html(updateTimePlaylistTemporaryFunction($(this).data("time")));
+  });
 }
