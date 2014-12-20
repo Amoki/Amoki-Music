2e0d78ddcaeff295e5fed1598a8d64f5e613a8c6
Amoki <hugoduroux@gmail.com>
2014-12-20 11:34:56 +0100

    indent using spaces

--- player/views/login.py
+++ player/views/login.py
@@ -4,26 +4,26 @@ from player.models import Room
 
 
 def login(request):
-	if request.session.get('room', False):
-		return redirect('remote', permanent=True)
-	room_name = request.POST.get('room')
-	password = request.POST.get('password')
-	if request.method == "POST" and room_name and password:
-		room = Room.objects.filter(name=room_name)
-		if room.count() == 0:
-			bad_room = True
-		elif room[0].password != password:
-			bad_password = True
-		else:
-			request.session['room'] = room_name
-			request.session['token'] = room[0].token
-			return redirect('remote', permanent=True)
+    if request.session.get('room', False):
+        return redirect('remote', permanent=True)
+    room_name = request.POST.get('room')
+    password = request.POST.get('password')
+    if request.method == "POST" and room_name and password:
+        room = Room.objects.filter(name=room_name)
+        if room.count() == 0:
+            bad_room = True
+        elif room[0].password != password:
+            bad_password = True
+        else:
+            request.session['room'] = room_name
+            request.session['token'] = room[0].token
+            return redirect('remote', permanent=True)
 
-	rooms = Room.objects.values_list('name', flat=True).all()
-	return render(request, 'login.html', locals())
+    rooms = Room.objects.values_list('name', flat=True).all()
+    return render(request, 'login.html', locals())
 
 
 def logout(request):
-	request.session.flush()
-	rooms = Room.objects.values_list('name', flat=True).all()
-	return render(request, 'login.html', locals())
+    request.session.flush()
+    rooms = Room.objects.values_list('name', flat=True).all()
+    return render(request, 'login.html', locals())
--- player/views/viewer/host.py
+++ player/views/viewer/host.py
@@ -4,13 +4,13 @@ from player.models import Room
 
 
 def host(request):
-	if not request.session.get('room', False):
-		return redirect('login', permanent=True)
-	else:
-		room = Room.objects.get(name=request.session.get('room'))
-		current_music = room.current_music
-		if current_music:
-			current_time_past = room.get_current_time_past()
-			music_id = current_music.music_id
+    if not request.session.get('room', False):
+        return redirect('login', permanent=True)
+    else:
+        room = Room.objects.get(name=request.session.get('room'))
+        current_music = room.current_music
+        if current_music:
+            current_time_past = room.get_current_time_past()
+            music_id = current_music.music_id
 
-	return render(request, 'player.html', locals())
+    return render(request, 'player.html', locals())
