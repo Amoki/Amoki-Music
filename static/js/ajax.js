ed6c91d3f671f8275027e7189fa40e333b5fbd43
Eirika <pitchetrou@msn.com>
2015-01-09 17:46:20 +0100

    typo fix

--- static/js/ajax.js
+++ static/js/ajax.js
@@ -162,7 +162,7 @@ $(document).on('submit', '.ajax_music_inifite_scroll', function(e) {
   var form =  $(this);
   var urlSubmit = form.attr('action');
   var dataSend = 'page=' + encodeURIComponent(form.children("#page").val());
-  $("<li id='spinner_Library' class='list-group-item item-lib library-list-music row row-list-item' style='color:black'><i class='fa fa-spinner fa-4x fa-spin'></i></li>").insertBefore(form.closest('li'));
+  $("<li id='spinner_library' class='list-group-item item-lib library-list-music row row-list-item' style='color:black'><i class='fa fa-spinner fa-4x fa-spin'></i></li>").insertBefore(form.closest('li'));
 
   $.ajax({
     type: "POST",
@@ -177,7 +177,7 @@ $(document).on('submit', '.ajax_music_inifite_scroll', function(e) {
       else {
         form.children("#page").addClass('disabled');
       }
-      $("#spinner_Library").remove();
+      $("#spinner_library").remove();
     },
     error: function(resultat, statut, erreur) {
       log_errors(resultat, statut, erreur);
