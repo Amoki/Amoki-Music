8e6f0d52ab059f7529d57c55a8d58cfd32eb5169
Eirika <pitchetrou@msn.com>
2015-03-13 04:04:14 +0100

    No debug in prod

--- amoki_music/settings/production.py
+++ amoki_music/settings/production.py
@@ -9,9 +9,9 @@ BASE_DIR = os.path.normpath(os.path.dirname(os.path.abspath(__file__)) + "/../..
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = False
 
 INSTALLED_APPS += (
     'gunicorn',
