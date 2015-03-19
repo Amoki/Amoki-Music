60000ccd644e823a3fc0635d4a316b727af7ae08
Eirika <pitchetrou@msn.com>
2015-03-19 14:45:42 +0100

    1 week rollback..

--- static/js/remote/ajax.js
+++ static/js/remote/ajax.js
@@ -14,37 +14,55 @@ beforeSend: function(xhr, settings) {
 
 $(document).on('submit', '.ajax-shuffle', function(e) {
   e.preventDefault();
-  var $this = $(this);
-  ajax($this).done(function(data) {
-    if(data.shuffle === true) {
-      modal_confirm($('#modal-shuffle-on'));
-    }
-    else {
-      modal_confirm($('#modal-shuffle-off'));
-    }
-  })
-  .fail(function(resultat, statut, erreur) {
-    log_errors(resultat, statut, erreur);
+  var form =  $(this);
+  var urlSubmit = form.attr('action');
+  var dataSend = 'shuffle=' + encodeURIComponent(form.children("button").val());
+
+  $.ajax({
+    type: "POST",
+    url: urlSubmit,
+    data: dataSend,
+    dataType: "json",
+    success: function(data) {
+      if(data.shuffle === true) {
+        modal_confirm($('#modal-shuffle-on'));
+      }
+      else {
+        modal_confirm($('#modal-shuffle-off'));
+      }
+    },
+    error: function(resultat, statut, erreur) {
+      log_errors(resultat, statut, erreur);
+    },
   });
 });
 
 
 $(document).on('submit', '.ajax-next, .ajax-dead-link', function(e) {
   e.preventDefault();
-  var $this = $(this);
-  ajax($this).done(function(data) {
-    modal_confirm($('#modal-next-music'));
-  })
-  .fail(function(resultat, statut, erreur) {
-    log_errors(resultat, statut, erreur);
+  var form =  $(this);
+  var urlSubmit = form.attr('action');
+  var dataSend = 'music_id=' + encodeURIComponent(form.children('.music_id').val());
+
+  $.ajax({
+    type: "POST",
+    url: urlSubmit,
+    data: dataSend,
+    dataType: "json",
+    success: function(data) {
+      modal_confirm($('#modal-next-music'));
+    },
+    error: function(resultat, statut, erreur) {
+      log_errors(resultat, statut, erreur);
+    },
   });
 });
 
 $(document).on('submit', '.ajax-search', function(e) {
   e.preventDefault();
-  var $this = $(this);
+  var form =  $(this);
 
-  if($this.children('.query').val().trim() === '' || $this.children('.query').val().trim() === null) {
+  if(form.children('.query').val().trim() === '' || form.children('.query').val().trim() === null) {
     $("#list-youtube").slideUp();
     $("#list-youtube").promise().done(function() {
       $("#list-youtube").children().remove();
@@ -53,82 +71,117 @@ $(document).on('submit', '.ajax-search', function(e) {
     });
     return;
   }
-  $this.children("span").children("button").children("i").attr("class", "fa fa-refresh fa-spin");
-  $this.children("span").children("button").attr('disabled', 'disabled');
+  form.children("span").children("button").children("i").attr("class", "fa fa-refresh fa-spin");
+  form.children("span").children("button").attr('disabled', 'disabled');
+  var urlSubmit = form.attr('action');
+  var dataSend = 'query=' + encodeURIComponent(form.children('.query').val());
 
-  ajax($this).done(function(data) {
-    if(data.current_music) {
-      $("#btn-search").children("i").attr("class", "fa fa-youtube-play");
-      $("#btn-search").removeAttr('disabled');
-      modal_confirm($('#modal-add-music'));
-    }
-    else {
-      $("#tab_btn_youtube").addClass("active");
-      $("#youtube").addClass("active");
-      $("#tab_btn_library").removeClass("active");
-      $("#library").removeClass("active");
-      $("#list-youtube").slideUp();
-      $("#list-youtube").promise().done(function() {
-        $(".youtube-list-music").remove();
-        $("#list-youtube").html(data.template_library);
-        $("#list-youtube").slideDown();
-        $("#list-youtube").promise().done(function() {
+  $.ajax({
+      type: "POST",
+      url: urlSubmit,
+      data: dataSend,
+      dataType: "json",
+      success: function(data) {
+        if(data.current_music) {
           $("#btn-search").children("i").attr("class", "fa fa-youtube-play");
           $("#btn-search").removeAttr('disabled');
-        });
-      });
-    }
-  })
-  .fail(function(resultat, statut, erreur) {
-    log_errors(resultat, statut, erreur);
+          modal_confirm($('#modal-add-music'));
+        }
+        else {
+          $("#tab_btn_youtube").addClass("active");
+          $("#youtube").addClass("active");
+          $("#tab_btn_library").removeClass("active");
+          $("#library").removeClass("active");
+          $("#list-youtube").slideUp();
+          $("#list-youtube").promise().done(function() {
+            $(".youtube-list-music").remove();
+            $("#list-youtube").html(data.template_library);
+            $("#list-youtube").slideDown();
+            $("#list-youtube").promise().done(function() {
+              $("#btn-search").children("i").attr("class", "fa fa-youtube-play");
+              $("#btn-search").removeAttr('disabled');
+            });
+          });
+        }
+      },
+      error: function(resultat, statut, erreur) {
+        log_errors(resultat, statut, erreur);
+      },
   });
 });
 
 $(document).on('submit', '.ajax-add-music', function(e) {
   e.preventDefault();
-  var $this =  $(this);
-  $this.children("button").children("span").attr("class", "fa fa-refresh fa-spin");
-  $this.children("button").attr('disabled', 'disabled');
+  var form =  $(this);
+  var urlSubmit = form.attr('action');
+  var dataSend = {
+    'music_id': encodeURIComponent($(this).children('.music_id').val()),
+    'requestId': encodeURIComponent($(this).children('.requestId').val()),
+  };
+  form.children("button").children("span").attr("class", "fa fa-refresh fa-spin");
+  form.children("button").attr('disabled', 'disabled');
 
-  ajax($this).done(function(data) {
-    $this.children("button").children("span").attr('class', 'glyphicon glyphicon-headphones');
-    $this.children("button").removeAttr('disabled');
-    modal_confirm($('#modal-add-music'));
-  })
-  .fail(function(resultat, statut, erreur) {
-    log_errors(resultat, statut, erreur);
+  $.ajax({
+      type: "POST",
+      url: urlSubmit,
+      data: dataSend,
+      dataType: "json",
+      success: function(data) {
+        form.children("button").children("span").attr('class', 'glyphicon glyphicon-headphones');
+        form.children("button").removeAttr('disabled');
+        modal_confirm($('#modal-add-music'));
+      },
+      error: function(resultat, statut, erreur) {
+        log_errors(resultat, statut, erreur);
+      },
   });
 });
 
 $(document).on('submit', '.ajax-volume', function(e) {
   e.preventDefault();
-  var $this = $(this);
+  var form =  $(this);
+  var urlSubmit = form.attr('action');
+  var dataSend = 'volume_change=' + encodeURIComponent(form.children(".volume_clicked").val());
 
-  ajax($this).done(function(data) {
-    $this.children(".volume_clicked").removeClass("volume_clicked");
-  })
-  .fail(function(resultat, statut, erreur) {
-    log_errors(resultat, statut, erreur);
+  $.ajax({
+    type: "POST",
+    url: urlSubmit,
+    data: dataSend,
+    dataType: "json",
+    success: function(data) {
+      form.children(".volume_clicked").removeClass("volume_clicked");
+    },
+    error: function(resultat, statut, erreur) {
+      log_errors(resultat, statut, erreur);
+    },
   });
 });
 
 $(document).on('submit', '.ajax_music_inifite_scroll', function(e) {
   e.preventDefault();
-  var $this = $(this);
-  $("<li id='spinner_library' class='list-group-item item-lib row row-list-item' style='color:black'><i class='fa fa-spinner fa-4x fa-spin'></i></li>").insertBefore($this.closest('li'));
+  var form =  $(this);
+  var urlSubmit = form.attr('action');
+  var dataSend = 'page=' + encodeURIComponent(form.children("#page").val());
+  $("<li id='spinner_library' class='list-group-item item-lib row row-list-item' style='color:black'><i class='fa fa-spinner fa-4x fa-spin'></i></li>").insertBefore(form.closest('li'));
 
-  ajax($this).done(function(data) {
-    $(data.template).insertBefore($this.closest('li'));
+  $.ajax({
+    type: "POST",
+    url: urlSubmit,
+    data: dataSend,
+    dataType: "json",
+    success: function(data) {
+      $(data.template).insertBefore(form.closest('li'));
       if(data.more_musics) {
-        $this.children("#page").val(parseInt($this.children("#page").val()) + 1);
+        form.children("#page").val(parseInt(form.children("#page").val()) + 1);
       }
       else {
-        $this.children("#page").addClass('disabled');
+        form.children("#page").addClass('disabled');
       }
       $("#spinner_library").remove();
-  })
-  .fail(function(resultat, statut, erreur) {
-    log_errors(resultat, statut, erreur);
+    },
+    error: function(resultat, statut, erreur) {
+      log_errors(resultat, statut, erreur);
+    },
   });
 });
 
@@ -144,11 +197,11 @@ function update_player() {
     success: function(data) {
       if(data.shuffle === true) {
         $("#btn-shuffle").attr("value", "false");
-        $("#submit-shuffle").attr("class", "btn btn-default btn-control btn-shuffle-true");
+        $("#btn-shuffle").attr("class", "btn btn-default btn-control btn-shuffle-true");
       }
       else {
         $("#btn-shuffle").attr("value", "true");
-        $("#submit-shuffle").attr("class", "btn btn-default btn-control btn-shuffle-false");
+        $("#btn-shuffle").attr("class", "btn btn-default btn-control btn-shuffle-false");
       }
 
       $('.library-list-music').remove();
@@ -174,17 +227,8 @@ function log_errors(resultat, statut, erreur){
   console.log("Error : "+erreur);
 }
 
-function ajax(source){
-  return $.ajax({
-    url: source.attr('action'),
-    type: source.attr('method'),
-    data: source.serialize().replace(/.[^&]+=\.?(?:&|$)/gm, ''),
-    dataType: "json",
-  });
-}
-
 socket.on('message', function(message) {
-  if(message.update === true){
+  if(message.update == 'true'){
     update_player();
   }
 });
---
+++ static/js/remote/timer.js
@@ -0,0 +1,18 @@
+$( document ).ready(function() {
+  $('#time-left-progress-bar').countdown('destroy');
+  $.countdown.setDefaults({
+    compact: true,
+  });
+});
+
+
+function timeline(current_time_left, current_time_past_percent){
+  $(".progress-bar").stop();
+  var actual_time = current_time_left;
+  actual_time *= 1000;
+  $(".progress-bar").width(current_time_past_percent+'%');
+  $(".progress-bar").animate({ 'width' : '100%'} , {
+      duration: actual_time,
+      easing: 'linear',
+  });
+}
