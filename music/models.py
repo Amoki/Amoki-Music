779b8d0612c7e90ff3c96af1036bd5aad8169736
Eirika <pitchetrou@msn.com>
2015-03-21 02:28:03 +0100

    Add signal update duration + comment + migration

--- music/models.py
+++ music/models.py
@@ -16,26 +16,18 @@ class Music(models.Model):
     last_play = models.DateTimeField(null=True)
     # signalement de lien mort
     dead_link = models.BooleanField(default=False)
-    timer_start = models.PositiveIntegerField(editable=False, default=0)
-    timer_end = models.PositiveIntegerField(editable=False, null=True)
+    timer_start = models.PositiveIntegerField(default=0)
+    timer_end = models.PositiveIntegerField(null=True)
 
     @classmethod
     def add(cls, **kwargs):
         existing_music = Music.objects.filter(music_id=kwargs['music_id'], room=kwargs['room']).first()
         if existing_music:
-            if existing_music.timer_end:
-                duration = existing_music.duration - (existing_music.duration - existing_music.timer_end)
-            duration = existing_music.duration - existing_music.timer_start
-            if existing_music.duration != duration:
-                existing_music.duration = duration
             existing_music.date = datetime.now()
             existing_music.save()
             return existing_music
         else:
             music = cls(**kwargs)
-            if music.timer_end:
-                music.duration = music.duration - (music.duration - music.timer_end)
-            music.duration = music.duration - music.timer_start
             music.save()
             return music
 
--- player/signals.py
+++ player/signals.py
@@ -2,6 +2,7 @@
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
 from player.models import Room, events, generate_token
+from music.models import Music
 
 
 @receiver(pre_save, sender=Room)
@@ -11,6 +12,18 @@ def add_event(sender, instance, **kwargs):
 
 @receiver(pre_save, sender=Room)
 def update_token_on_password_change(sender, instance, **kwargs):
+    # first save() of a room don't have pk (not in BD).
+    # This if is used to skip the signal of this first call.
+    # (Room.objects.get need to have a valid pk argument)
     if instance.pk:
         if instance.password != Room.objects.get(pk=instance.pk).password:
             instance.token = generate_token()
+
+@receiver(pre_save, sender=Music)
+def update_duration(sender, instance, **kwargs):
+    if instance.timer_end:
+        new_duration = instance.duration - (instance.duration - instance.timer_end)
+    new_duration = instance.duration - instance.timer_start
+
+    if new_duration != instance.duration:
+        instance.duration = new_duration
