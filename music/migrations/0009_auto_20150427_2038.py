6a2f7a24f9cc58d3202cfe88082925088ca20812
Amoki <hugo.duroux@gmail.com>
2015-04-27 22:53:10 +0200

    migration for urls

--- music/migrations/0009_auto_20150427_2038.py
+++ music/migrations/0009_auto_20150427_2038.py
@@ -8,9 +8,13 @@ def set_urls(apps, schema_editor):
     # We can't import the Person model directly as it may be a newer
     # version than this migration expects. We use the historical version.
     Music = apps.get_model("music", "Music")
+    TemporaryMusic = apps.get_model("music", "TemporaryMusic")
     for music in Music.objects.all():
         music.url = "https://www.youtube.com/watch?v=" + music.music_id
         music.save()
+    for tempMusic in TemporaryMusic.objects.all():
+        tempMusic.url = "https://www.youtube.com/watch?v=" + tempMusic.music_id
+        tempMusic.save()
 
 
 class Migration(migrations.Migration):
@@ -25,10 +29,20 @@ class Migration(migrations.Migration):
             name='url',
             field=models.CharField(max_length=512, editable=False, null=True),
         ),
+        migrations.AddField(
+            model_name='temporarymusic',
+            name='url',
+            field=models.CharField(max_length=512, null=True),
+        ),
         migrations.RunPython(set_urls),
         migrations.AlterField(
             model_name='music',
             name='url',
             field=models.CharField(max_length=512, editable=False),
+        ),
+        migrations.AlterField(
+            model_name='temporarymusic',
+            name='url',
+            field=models.CharField(max_length=512),
         ),
     ]
