069e0802c66ac78d868a5658f668faf93d5b6902
Eirika <pitchetrou@msn.com>
2015-05-01 15:58:01 +0200

    Fix csrf error dead-link on player

---
+++ static/js/player/ajax.js
@@ -0,0 +1,34 @@
+var csrftoken = $.cookie('csrftoken');
+function csrfSafeMethod(method) {
+  // these HTTP methods do not require CSRF protection
+  return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+$.ajaxSetup({
+beforeSend: function(xhr, settings) {
+  if(!csrfSafeMethod(settings.type) && !this.crossDomain) {
+    xhr.setRequestHeader("X-CSRFToken", csrftoken);
+  }
+}
+});
+
+
+$(document).on('submit', '.ajax-dead-link', function(e) {
+  e.preventDefault();
+  var $this = $(this);
+  ajax($this).fail(log_errors);
+});
+
+function ajax(source){
+  return $.ajax({
+    url: source.attr('action'),
+    type: source.attr('method'),
+    data: source.serialize().replace(/[^&]+=(?:&|$)/gm, ''),
+    dataType: "json",
+  });
+}
+
+function log_errors(resultat, statut, erreur){
+  console.error(resultat.responseText);
+  console.error("Statut : " + statut);
+  console.error("Error: " + erreur.stack);
+}
