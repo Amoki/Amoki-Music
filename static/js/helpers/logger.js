4e5e5c49760ed2f4fb2d2ce21feae8420145ee1c
Amoki <hugo.duroux@gmail.com>
2015-08-16 00:54:25 +0200

    remove fail files

--- static/js/helpers/logger.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-module.exports.error = function logError(resultat, statut, erreur) {
-  console.error(resultat.responseText);
-  console.error("Statut : " + statut);
-  console.error("Error: " + erreur.stack);
-};
