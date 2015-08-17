e7230a317739f275321f4261ad23686ff1bafc3e
Amoki <hugo.duroux@gmail.com>
2015-08-17 02:02:18 +0200

    fix production

--- amoki_music/settings/common.py
+++ amoki_music/settings/common.py
@@ -103,7 +103,7 @@ PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = BASE_DIR + '/collected-static'
+STATIC_ROOT = BASE_DIR + '/../collected-static'
 
 # URL prefix for static files.
 # Example: "http://media.l
@@ -114,7 +114,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    'static',
+    'website/static',
 )
 
 # List of finder classes that know how to find static files in
