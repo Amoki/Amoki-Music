00aa35f6f49e266c3e8a625c54756855918015fd
Amoki <hugo.duroux@gmail.com>
2015-09-16 22:15:58 +0200

    delete and patch music

--- music/serializers.py
+++ music/serializers.py
@@ -9,6 +9,7 @@ class MusicSerializer(serializers.ModelSerializer):
     """
     Serializing all the Music
     """
+
     class Meta:
         model = Music
         fields = ('music_id', 'name', 'thumbnail', 'count', 'duration', 'timer_start', 'timer_end')
