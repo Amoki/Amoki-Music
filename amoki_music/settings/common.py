fbfbf6a6cdf12934e820fd534fc4e92d48d5f585
Amoki <hugo.duroux@gmail.com>
2015-09-23 00:17:29 +0200

    add admin doc

--- amoki_music/settings/common.py
+++ amoki_music/settings/common.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.admindocs',
     # Pip lib
     'ws4redis',
     'rest_framework',
--- amoki_music/urls.py
+++ amoki_music/urls.py
@@ -5,6 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^docs/', include('rest_framework_swagger.urls')),
     url(r'^', include('endpoints.routes')),
 )
