c5073dcd655c5d5201a2b05953e7dda4cad6a141
Amoki <hugo.duroux@gmail.com>
2015-09-23 22:36:03 +0200

    add comment about little hack

--- music/admin.py
+++ music/admin.py
@@ -13,6 +13,7 @@ class MusicAdmin(admin.ModelAdmin):
 
     def add_music(self, request, queryset):
         for music in queryset:
+            # Hack to transform music into dict
             music.room.add_music(**MusicSerializer(music).data)
         return
 
