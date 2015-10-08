63b76d09c8113c7386832df160305271c71cc62f
Amoki <hugo.duroux@gmail.com>
2015-10-08 23:11:54 +0200

    improve music_endpoint tests

--- endpoints/tests/test_music.py
+++ endpoints/tests/test_music.py
@@ -72,6 +72,27 @@ class TestMusic(EndpointTestCase):
         m.timer_end.should.eql(100)
         m.duration.should.eql(92)
 
+    def test_patch_unexisting_music(self):
+        response = self.client.patch('/music/165423123', {'timer_start': 8, "timer_end": 100})
+
+        response.status_code.should.eql(status.HTTP_404_NOT_FOUND)
+
+    def test_patch_bad_arguments(self):
+        m = Music(
+            music_id="a",
+            name="a",
+            thumbnail="https://a.com",
+            duration=114,
+            url="https://www.a.com",
+            source="youtube",
+            room=self.r,
+        )
+        m.save()
+
+        response = self.client.patch('/music/' + str(m.pk), {'duration': 'wtf'})
+
+        response.status_code.should.eql(status.HTTP_400_BAD_REQUEST)
+
     def test_post(self):
         music_to_post = {
             "music_id": "a",
@@ -87,3 +108,13 @@ class TestMusic(EndpointTestCase):
         response.status_code.should.eql(status.HTTP_201_CREATED)
 
         Music.objects.filter(music_id='a', room=self.r).exists().should.be.true
+
+    def test_post_invalid_data(self):
+        music_to_post = {
+            "music_id": "a",
+            "lolilol": "coucou"
+        }
+
+        response = self.client.post('/music', music_to_post)
+
+        response.status_code.should.eql(status.HTTP_400_BAD_REQUEST)
