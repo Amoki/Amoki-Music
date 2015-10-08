f3abc76309d9260aa23d1110482999cde33ae780
Amoki <hugo.duroux@gmail.com>
2015-10-08 23:38:02 +0200

    remove useless check in decorator

--- endpoints/utils/decorators.py
+++ endpoints/utils/decorators.py
@@ -5,12 +5,7 @@ from endpoints.utils.authentication import authenticate
 
 def room_required(func):
     @wraps(func)
-    def wrap(self, request=None, **kwargs):
-        if not request:
-            request = self
-            self = None
+    def wrap(self, request, **kwargs):
         room = authenticate(request)
-        if not self:
-            return func(request, room=room, **kwargs)
         return func(self, request, room=room, **kwargs)
     return wrap
