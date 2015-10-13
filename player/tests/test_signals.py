cbb62604b0cd495ec9d0fd64ca96d1d50e48df2f
Amoki <hugo.duroux@gmail.com>
2015-10-13 22:54:46 +0200

    use sure for signals tests

--- player/tests/test_signals.py
+++ player/tests/test_signals.py
@@ -1,6 +1,8 @@
 from utils.testcase import TestCase
 from player.models import Events, Room
 
+import sure
+
 
 class TestSignals(TestCase):
     def test_update_token_on_password_change(self):
@@ -9,7 +11,7 @@ class TestSignals(TestCase):
         self.r.password = 'b'
         self.r.save()
 
-        self.assertNotEqual(self.r.token, first_token)
+        self.r.token.should_not.eql(first_token)
 
     def test_create_room_event(self):
         Room(name='b', password='b').save()
