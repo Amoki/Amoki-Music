c46191dded67106dd4829bbc532f48e363a8a2bf
Eirika <pitchetrou@msn.com>
2015-11-16 09:35:01 +0100

    Add docstring to create/update serializer methods

--- music/serializers.py
+++ music/serializers.py
@@ -20,11 +20,41 @@ class MusicSerializer(serializers.ModelSerializer):
         fields = ('pk', 'music_id', 'name', 'thumbnail', 'count', 'duration', 'total_duration', 'timer_start', 'url', 'room_id', 'source', 'last_play')
 
     def create(self, validated_data):
+        """
+            Override the default Serializer.save() method
+            =============================================
+
+            create(..) is called if we don't pass an existing music to the Serializer
+            update(..) method NEED to be implemented too !
+
+            :param self: Instance param
+            :param validated_data: A dict of validated data from the Serializer
+            :type self: MusicSerializer
+            :type validated_data: dict
+            :return: The Music object CREATED with the valid data from the Serializer
+            :rtype: Music
+        """
         music = Music(**validated_data)
         music.save()
         return music
 
     def update(self, instance, validated_data):
+        """
+            Override the default Serializer.save() method
+            =============================================
+
+            update(..) is called if we pass an existing music to the Serializer
+            create(..) method NEED to be implemented too !
+
+            :param self: Instance param
+            :param instance: The Music instance given to the Serializer
+            :param validated_data: A dict of validated data from the Serializer
+            :type self: MusicSerializer
+            :type instance: Music
+            :type validated_data: dict
+            :return: The Music object UPDATED with the valid data from the Serializer
+            :rtype: Music
+        """
         Music.objects.filter(pk=instance.pk).update(**validated_data)
         return instance
 
