c501bba80fbf9d5a85c82e1d047d4c1d90e5a80d
Amoki <hugo.duroux@gmail.com>
2016-01-07 20:39:45 +0100

    remove useless Class

--- endpoints/utils/paginators.py
+++ endpoints/utils/paginators.py
@@ -1,12 +1,6 @@
 from rest_framework.pagination import PageNumberPagination
 
 
-class LargeResultsSetPagination(PageNumberPagination):
-    page_size = 1000
-    page_size_query_param = 'page_size'
-    max_page_size = 10000
-
-
 class StandardResultsSetPagination(PageNumberPagination):
     page_size = 40
     page_size_query_param = 'page_size'
