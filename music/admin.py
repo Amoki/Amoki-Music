e668ef987e9504acd2b9c3ce32deb1b95612966d
TomYoYo <tom.pueyo@gmail.com>
2016-02-22 11:25:34 +0100

    add action to be tested

--- music/admin.py
+++ music/admin.py
@@ -1,13 +1,22 @@
 from django.contrib import admin
 from music.models import Music
+from player.models import Room
+from django.contrib.admin.helpers import ActionForm
+from django import forms
+from django.db import models
 
 from music.serializers import MusicSerializer
+
+class UpdateActionForm(ActionForm):
+    room = models.CharField(choices=Room.objects.all(),)
+
 
 
 class MusicAdmin(admin.ModelAdmin):
     list_display = ('name', 'count', 'music_id', 'source', 'duration', 'last_play', 'thumbnail', 'room', 'timer_start')
-    actions = ('add_music',)
+    actions = ('add_music','duplicate_music')
 
+    action_form = UpdateActionForm
     def has_add_permission(self, request):
         return False
 
@@ -16,5 +25,14 @@ class MusicAdmin(admin.ModelAdmin):
             # Hack to transform music into dict
             music.room.add_music(**MusicSerializer(music).data)
         return
+
+    def duplicate_music(self,request,queryset):
+        room = request.POST['room']
+        for music in queryset:
+            room.add_music(**MusicSerializer(music).data)
+        return
+
+
+
 
 admin.site.register(Music, MusicAdmin)
