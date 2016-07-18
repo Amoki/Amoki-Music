466a2718e86895c3076a138a9143a4bfd47ebd2d
Eirika <pitchetrou@msn.com>
2016-07-18 16:17:04 +0200

    Update playlistTrack admin
    playlistTrack are now listed in the corresponding room

--- player/admin.py
+++ player/admin.py
@@ -1,15 +1,26 @@
 from django.contrib import admin
 from player.models import Room, PlaylistTrack
 
-from ordered_model.admin import OrderedModelAdmin
+from ordered_model.admin import OrderedTabularInline
+
+
+class PlaylistTrackAdmin(OrderedTabularInline):
+    model = PlaylistTrack
+    fields = ('order', 'move_up_down_links', 'track_type', 'track')
+    readonly_fields = ('order', 'move_up_down_links', 'room')
+    extra = 0
+    ordering = ('track_type', 'order',)
 
 
 class RoomAdmin(admin.ModelAdmin):
     list_display = ('name', 'shuffle', 'current_music', 'can_adjust_volume')
+    inlines = (PlaylistTrackAdmin, )
 
-
-class ItemAdmin(OrderedModelAdmin):
-    list_display = ('move_up_down_links', 'order', 'track', 'room')
+    def get_urls(self):
+        urls = super(RoomAdmin, self).get_urls()
+        for inline in self.inlines:
+            if hasattr(inline, 'get_urls'):
+                urls = inline.get_urls(self) + urls
+        return urls
 
 admin.site.register(Room, RoomAdmin)
-admin.site.register(PlaylistTrack, ItemAdmin)
