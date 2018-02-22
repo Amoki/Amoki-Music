77b0e426fe9cc6c9cd12346a5e5e81a62362bb83
Amoki <hugo.duroux@gmail.com>
2018-02-22 15:19:41 +0100

    fix middleware config

--- amoki_music/settings.py
+++ amoki_music/settings.py
@@ -66,7 +66,7 @@ INSTALLED_APPS = (
     'website',
 )
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
